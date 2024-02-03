e0b962fb48742a139d1ec2b59344d0f8ab7dd782
SR <>
2024-02-03 00:13:08 -0800

    Moved pin data setter/getter to data pin class

--- node_editor/data_pin.py
+++ node_editor/data_pin.py
@@ -5,3 +5,11 @@ class DataPin(Pin):
     def __init__(self, parent, scene):
         super().__init__(parent, scene)
         self.set_execution(False)
+        self._data = None
+
+    @property
+    def data(self):
+        return self._data
+    
+    def set_data(self, data):
+        self._data = data
